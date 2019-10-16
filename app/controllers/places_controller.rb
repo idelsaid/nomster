59ea77672196373c8424ff2a792c0cb35a5a44a3
IdelSaid <idelsaid@gmail.com>
2019-10-16 01:55:39 -0500

    validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController 
-    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(page: params[:page])
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
 
 
   def create
-  current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -22,29 +26,33 @@ class PlacesController < ApplicationController
   def edit
     @place = Place.find(params[:id])
 
-  if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
-   end
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
-  @place = Place.find(params[:id])
-  if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
-  end
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
 
-  @place.update_attributes(place_params)
-  redirect_to root_path
+    @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
-  @place = Place.find(params[:id])
-  if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
-  end
-  
-  @place.destroy
-  redirect_to root_path
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+    
+    @place.destroy
+    redirect_to root_path
   end
 
   private
